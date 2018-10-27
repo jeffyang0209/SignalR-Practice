4df67b34d0ebfa3a8313f6b7feda6654a028521d
jeffyang0209 <jeffyang0209@gmail.com>
2018-10-27 19:16:19 +0800

    建立Server端的函式

---
+++ SignalR/SignalR/Hubs/Hello.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Hubs
+{
+    public class Hello : Hub
+    {
+        // 另一種ReName的方式
+        // [HubName("Hello")]
+        public void HelloWord()
+        { 
+            // 執行Cleint端的JS函式名稱
+            Clients.All.hello();
+        }
+    }
+}
