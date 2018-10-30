e92ac8e75d8857394a48f18783c5a3e30b1d2402
jeffyang0209 <jeffyang0209@gmail.com>
2018-10-30 15:24:14 +0800

    引入Vue，以及Client+Server端溝通實做

---
+++ SignalR/SignalR/Hubs/Chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalR.Hubs
+{
+    public class Chat : Hub
+    {
+        public void SendMessage(string userName, string message)
+        {
+            Clients.All.recievMessage(userName, message);
+        }
+    }
+}
